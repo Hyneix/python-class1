a332de4b8d3800eefb80791e857c4515a23c9374
Hyneix <karkisushant08@gmail.com>
2022-06-23 18:16:22 +0545

    New

---
+++ OOP.py
@@ -0,0 +1,34 @@
+# class calculator:
+#
+#     def addition(self, x, y):
+#         print(f'The addition of {x} and {y} is, x+y')
+#
+#     def subtraction(self, x, y):
+#         print(f'The subtraction between {x} and {y} is ', x-y)
+#
+#     def multiplication(self, x, y):
+#         print(f'The multiplication of {x} and {y} is ', x*y)
+#
+#     def division(self, x, y):
+#         print(f'The division of {x} by {y} is ', x/y)
+#
+#
+# obj = calculator()
+# obj.addition(10, 5)
+# obj.subtraction(10, 5)
+# obj.division(10, 5)
+# obj.multiplication(10, 5)
+
+
+class name:
+
+    def __int__(self, fname, age):
+        self.fname = fname
+
+    def speak(self):
+        print(f'My name is {self.fname}.')
+
+
+obj = name()
+obj.__int__('Ram',25)
+obj.speak()
